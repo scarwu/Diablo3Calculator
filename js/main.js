3b9f049581ae6e05678d4e790aa6e7342cbcb4b9
ScarWu <xneriscool@gmail.com>
2012-07-07 02:05:20 +0800

    fix bug

--- js/main.js
+++ js/main.js
@@ -119,6 +119,7 @@ $(function() {
 		
 		// Reset field
 		$('input').val(0);
+		$('.minor_dps').text(0);
 
 		// Set default point
 		$.each(default_point, function(key, value) {
@@ -154,6 +155,10 @@ $(function() {
 		totalAttribute(replacement, 'replacement');
 		calculate(based, 'based');
 		calculate(replacement, 'replacement');
+		
+		// Minor DPS
+		calculateMinorDPS('based');
+		//calculateMinorDPS('replacement');
 
 		// If User change character profile
 		$('.character .order').change(function() {
@@ -167,6 +172,10 @@ $(function() {
 			totalAttribute(replacement, 'replacement');
 			calculate(based, 'based');
 			calculate(replacement, 'replacement');
+			
+			// Minor DPS
+			calculateMinorDPS('based');
+			//calculateMinorDPS('replacement');
 			
 			// Save data
 			saveProfile();
@@ -192,6 +201,10 @@ $(function() {
 			diff();
 			calculate(based, 'based');
 			calculate(replacement, 'replacement');
+			
+			// Minor DPS
+			calculateMinorDPS('based');
+			//calculateMinorDPS('replacement');
 		});
 	
 		$('#equip .based input').change(function() {
@@ -216,6 +229,10 @@ $(function() {
 			totalAttribute(replacement, 'replacement');
 			calculate(based, 'based');
 			calculate(replacement, 'replacement');
+			
+			// Minor DPS
+			calculateMinorDPS('based');
+			//calculateMinorDPS('replacement');
 		});
 
 		// Replacement
@@ -302,8 +319,6 @@ $(function() {
 				replacement[key] = based[key];
 			}
 		});
-		
-		// l(replacement);
 	}
 
 	function totalAttribute(data, target) {
@@ -331,6 +346,69 @@ $(function() {
 		$('#' + target + ' .total .max_damage').val(total['max_damage']);
 	}
 
+	function calculateMinorDPS(target) {
+		$.each($('#equip .' + target + ' > div'), function() {
+			var key = $(this).attr('class');
+			if(key != 'weapon_1' && key != 'weapon_2') {
+				var total = {
+					main_attribute: parseFloat($('#' + target + ' .attribute .main_attribute').val()),
+					critical_chance: parseFloat($('#' + target + ' .attribute .critical_chance').val()),
+					critical_damage: parseFloat($('#' + target + ' .attribute .critical_damage').val()),
+					attack_speed: parseFloat($('#' + target + ' .total .attack_speed').val()),
+					min_damage: parseFloat($('#' + target + ' .total .min_damage').val()),
+					max_damage: parseFloat($('#' + target + ' .total .max_damage').val())
+				};
+				
+				$.each($('#equip .' + target + ' .' + key + ' input'), function() {
+					total[$(this).attr('class')] -= parseFloat($(this).val());
+					
+				});
+				
+				var weapon_attack_per_second_1 = parseFloat($('#equip .' + target + ' .weapon_attack_per_second_1').val());
+				var weapon_min_damage_1 = parseFloat($('#equip .' + target + ' .weapon_min_damage_1').val());;
+				var weapon_max_damage_1 = parseFloat($('#equip .' + target + ' .weapon_max_damage_1').val());;
+				
+				var weapon_attack_per_second_2 = parseFloat($('#equip .' + target + ' .weapon_attack_per_second_2').val());;
+				var weapon_min_damage_2 = parseFloat($('#equip .' + target + ' .weapon_min_damage_2').val());;
+				var weapon_max_damage_2 = parseFloat($('#equip .' + target + ' .weapon_max_damage_2').val());;
+				
+				var main_attribute = total['main_attribute'];
+				var critical_chance = total['critical_chance'];
+				var critical_damage = total['critical_damage'];
+				var attack_speed = total['attack_speed'];
+				var min_damage = total['min_damage'];
+				var max_damage = total['max_damage'];
+				
+				var attack_per_second_1 = weapon_attack_per_second_1 * (attack_speed / 100 + 1);
+				var attack_per_second_2 = weapon_attack_per_second_2 * (attack_speed / 100 + 1);
+		
+				if(attack_per_second_2 != 0)
+					var average_aps = (attack_per_second_1 + attack_per_second_2) / 2;
+				else
+					var average_aps = attack_per_second_1;
+				
+				if(average_aps == 0)
+					average_aps = 1;
+				
+				// Skill bonus damage
+				var skill_damage = parseFloat($('#' + target + ' .skill .damage').val());
+				var dps = parseFloat($('#' + target + ' .attribute .damage_per_second').val());
+				
+				// Damage per second
+				var minor_dps = (weapon_min_damage_1 + weapon_max_damage_1 + weapon_min_damage_2 + weapon_max_damage_2) / 2;
+				minor_dps += (min_damage + max_damage) / 2;
+				minor_dps *= average_aps;
+				minor_dps *= main_attribute / 100 + 1;
+				minor_dps *= (critical_chance * critical_damage / 10000) + 1;
+				minor_dps *= skill_damage / 100 + 1;
+				minor_dps = dps - minor_dps;
+				minor_dps = parseInt(minor_dps, 10);
+
+				$('#equip .' + target + ' .' + key + ' .minor_dps').text(minor_dps + ' DPS');
+			}
+		});
+	}
+
 	function calculate(data, target) {
 		var main_attribute = parseFloat($('#' + target + ' .total .main_attribute').val());
 		main_attribute += parseFloat(default_point['main_attribute']);
@@ -353,13 +431,27 @@ $(function() {
 		$('#' + target + ' .attribute .critical_damage').val(critical_damage);
 		
 		// Weapon data
-		var weapon_attack_per_second_1 = parseFloat(data['weapon_1']['weapon_attack_per_second_1']) | 0;
-		var weapon_min_damage_1 = parseFloat(data['weapon_1']['weapon_min_damage_1']) | 0;
-		var weapon_max_damage_1 = parseFloat(data['weapon_1']['weapon_max_damage_1']) | 0;
+		var weapon_attack_per_second_1 = 0;
+		var weapon_min_damage_1 = 0;
+		var weapon_max_damage_1 = 0;
 		
-		var weapon_attack_per_second_2 = parseFloat(data['weapon_2']['weapon_attack_per_second_2']) | 0;
-		var weapon_min_damage_2 = parseFloat(data['weapon_2']['weapon_min_damage_2']) | 0;
-		var weapon_max_damage_2 = parseFloat(data['weapon_2']['weapon_max_damage_2']) | 0;
+		if(data['weapon_1']['weapon_attack_per_second_1'] != undefined)
+			weapon_attack_per_second_1 = parseFloat(data['weapon_1']['weapon_attack_per_second_1']);
+		if(data['weapon_1']['weapon_min_damage_1'] != undefined)
+			weapon_min_damage_1 = parseFloat(data['weapon_1']['weapon_min_damage_1']);
+		if(data['weapon_1']['weapon_max_damage_1'] != undefined)
+			weapon_max_damage_1 = parseFloat(data['weapon_1']['weapon_max_damage_1']);
+		
+		var weapon_attack_per_second_2 = 0;
+		var weapon_min_damage_2 = 0;
+		var weapon_max_damage_2 = 0;
+		
+		if(data['weapon_2']['weapon_attack_per_second_2'] != undefined)
+			weapon_attack_per_second_2 = parseFloat(data['weapon_2']['weapon_attack_per_second_2']);
+		if(data['weapon_2']['weapon_min_damage_2'] != undefined)
+			weapon_min_damage_2 = parseFloat(data['weapon_2']['weapon_min_damage_2']);
+		if(data['weapon_2']['weapon_max_damage_2'] != undefined)
+			weapon_max_damage_2 = parseFloat(data['weapon_2']['weapon_max_damage_2']);
 
 		var attack_per_second_1 = weapon_attack_per_second_1 * (attack_speed / 100 + 1);
 		var attack_per_second_2 = weapon_attack_per_second_2 * (attack_speed / 100 + 1);
@@ -417,7 +509,7 @@ $(function() {
 
 		// Display attribute
 		$('#' + target + ' .attribute .damage_per_second').val(dps);
-		$('#' + target + ' .attribute .normal_damage_range').val(min_normal_damage + ' ~ ' + max_normal_damage);
-		$('#' + target + ' .attribute .critical_damage_range').val(min_critical_damage + ' ~ ' + max_critical_damage);
+		$('#' + target + ' .attribute .normal_damage_range').val(min_normal_damage + ' - ' + max_normal_damage);
+		$('#' + target + ' .attribute .critical_damage_range').val(min_critical_damage + ' - ' + max_critical_damage);
 	}
 });
